1b048af3575a24bfde94d8f779943e16cdd803ff
Roger Melich <rogermelich@gmail.com>
2017-03-08 19:42:46 +0100

    Add Express example and node client example

---
+++ server/express.js
@@ -0,0 +1,6 @@
+var server = require('http').createServer();
+var io = require('socket.io')(server);
+io.on('connection', function(client){
+    client.on('event', function(data){});
+    client.on('disconnect', function(){});
+});
